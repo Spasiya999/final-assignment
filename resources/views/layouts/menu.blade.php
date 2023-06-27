aa4890d5515682413ffa58488d1a4b4e06bb0b54
Sachintha Pasindu <spsd49@gmail.com>
2023-06-27 20:43:35 +0530

    add dashboard menu some item

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,36 @@
 <!-- need to remove -->
 <li class="nav-item">
+    <a href="{{ route('dashboard') }}" class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>Web Site</p>
+    </a>
+</li>
+<li class="nav-item">
     <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
-        <p>Home</p>
+        <p>Web Site</p>
     </a>
+</li>
+<li class="nav-item">
+    <a href="#" class="nav-link active">
+        <i class="nav-icon fas fa-bed"></i>
+        <p>
+            Rooms
+            <i class="right fas fa-angle-left"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview" style="display: none;">
+        <li class="nav-item">
+            <a href="#" class="nav-link active">
+                <i class="fas fa-list-ul nav-icon"></i>
+                <p>Rooms List</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="fas fa-plus nav-icon"></i>
+                <p>Create Room</p>
+            </a>
+        </li>
+    </ul>
 </li>
