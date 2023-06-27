1e093ce57da9dee884d631b3fca32499e630387f
Sachintha Pasindu <spsd49@gmail.com>
2023-06-27 20:12:50 +0530

    add adminlte them for dahboard

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,11 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
